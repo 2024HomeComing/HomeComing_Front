c1a401881285b1903d185c3d26622b97d1653f7e
mlnj27 <yjm9575m89@gmail.com>
2024-09-28 00:15:14 +0900

    댓글 완료

--- app/src/main/java/com/example/practice1/dto/Scomment.java
+++ app/src/main/java/com/example/practice1/dto/Scomment.java
@@ -5,24 +5,15 @@ public class Scomment {
     private String writer;    // 작성자
     private String content;   // 댓글 내용
     private String time;      // 댓글 시간
-    private Long sightingId;     // 게시글 ID (어떤 게시글에 속하는 댓글인지)
+    private Long sightingId;  // 게시글 ID (어떤 게시글에 속하는 댓글인지)
     private String userId;    // 사용자 ID
 
     // 생성자
-    public Scomment(Long id, String writer, String content, String time, Long sightingId, String userId) {
-        this.id = id;
-        this.writer = writer;
-        this.content = content;
-        this.time = time;
-        this.sightingId = sightingId;
-        this.userId = userId;
-    }
-
     public Scomment(String userId, String content, String currentTime, Long sightingId) {
-    }
-
-
-    public Scomment(String content, long sightingId, String userId) {
+        this.userId = userId;
+        this.content = content;
+        this.time = currentTime; // 시간을 currentTime으로 설정
+        this.sightingId = sightingId;
     }
 
     // Getter & Setter 메서드
@@ -62,7 +53,7 @@ public class Scomment {
         return sightingId;
     }
 
-    public void setSightingId(Long sighitingId) {
+    public void setSightingId(Long sightingId) {
         this.sightingId = sightingId;
     }
 
